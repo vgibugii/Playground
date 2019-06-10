16e559cacf3a17fdbb3a3b5d2b09c8371075ec6a
vgibugii <51501046+vgibugii@users.noreply.github.com>
2019-06-10 17:57:02 +0530

    Test code pushed to repository.

---
+++ Area of a square/Main.java
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int area;
+  //area=9cm*9cm;
+  area=9*9;
+  printf("%d sq cm",area);
+  //Type your code here
+  return 0;
+}
---
+++ Greatest of two number/Main.java
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int num1,num2;
+  scanf("%d\n%d",&num1,&num2);
+  if(num1>num2)
+    printf("num1 is the greatest");
+  else
+    printf("num2 is the greatest");
+  //Type your code here
+  return 0;
+}
